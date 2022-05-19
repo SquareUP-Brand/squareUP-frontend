c67e296d230059610e3c6f3deb4485f706996953
Ryan Wendell <ryan@blubracket.com>
2022-05-19 15:23:43 -0500

    feat: create menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import HamburgerMenu from 'components/HamburgerMenu';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 
 const App = () => (
   <>
     <Header />
+    <HamburgerMenu />
     <Outlet />
   </>
 );
---
+++ src/components/HamburgerMenu.tsx
@@ -0,0 +1,7 @@
+const HamburgerMenu = () => (
+  <div>
+    "Test"
+  </div>
+);
+
+export default HamburgerMenu;
